43afbe644b2d68b9859e4ea81bdec0ca0d25cb7d
Aleksandr F <a7214370@gmail.com>
2022-07-01 12:07:05 +0300

    The app is complete.

--- DiceRandomApp/ViewController.swift
+++ DiceRandomApp/ViewController.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var diceImageLeft: UIImageView!
     @IBOutlet weak var diceImageRight: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        rollButton.layer.cornerRadius = 10
+        }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
